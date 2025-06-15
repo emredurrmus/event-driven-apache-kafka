446ce6d5d6f1400c4a03b90f4e5457dfd21d7019
emred <emredurmus0@hotmail.com>
2025-06-15 15:46:54 +0300

    feat: Group-Id Config

--- email-notification-service/src/main/java/com/emredurmus/ws/emailnotificationservice/handler/ProductCreatedEventHandler.java
+++ email-notification-service/src/main/java/com/emredurmus/ws/emailnotificationservice/handler/ProductCreatedEventHandler.java
@@ -8,13 +8,13 @@ import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 
 @Component
-@KafkaListener(topics = "product-created-topic")
+@KafkaListener(topics = "product-created-topic", groupId = "product-created-events")
 public class ProductCreatedEventHandler {
 
     private final Logger LOGGER = LoggerFactory.getLogger(this.getClass());
 
     @KafkaHandler
     public void handle(ProductCreatedEvent productCreatedEvent) {
-        LOGGER.info("ProductCreatedEvent: {}", productCreatedEvent.getTitle());
+        LOGGER.info("ProductCreatedEvent: {}", productCreatedEvent.getTitle() + "with product id: " + productCreatedEvent.getProductId());
     }
 }
